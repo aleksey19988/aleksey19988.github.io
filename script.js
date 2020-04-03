9664110f3cf2b887e0bf00d6988c62c6bf957010
Aleksey Kononenko <aleksey19988@gmail.com>
2020-04-03 10:47:56 +0300

    add js

---
+++ script.js
@@ -0,0 +1,13 @@
+let upButton = document.querySelector('.up_button');
+
+window.onscroll = function () {
+    if (window.pageYOffset > 300) {
+        upButton.classList.add('shown');
+    } else {
+        upButton.classList.remove('shown');
+    }
+};
+
+upButton.onclick = function () {
+    window.scrollTo(0, 0);
+}
